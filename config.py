ec2ab537e35e86b7edc30f8360c709ca169ac57c
Choi Yin Zheng <choiyz-wm23@student.tarc.edu.my>
2025-04-13 22:56:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cyqu1vg9nqwl.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "q*ES5Qt9qyUVDpEv(5X]0uY]E3-P"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "choiyz-employee"
 customregion = "us-east-1"
 
